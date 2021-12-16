ad7ad509feb67d4682c8df08cc52ef7ff9b3df9a
Connor McDonald <connormcd98@gmail.com>
2021-12-16 11:24:19 +0200

    complete

--- Markowitz_max_sharpe.py
+++ Markowitz_max_sharpe.py
@@ -14,4 +14,38 @@ S = risk_models.sample_cov(df)
 
 ef = EfficientFrontier(mu, S)
 weights = ef.max_sharpe()
+
+p_weights = []
+for i in weights:
+    p_weights.append(round(weights[i], 4))
+    v = round(100*weights[i],2)
+    if  v > 0:
+        print(f"{i}: {v}%\n")
+
+print('Training Period Perfomance:')
 ef.portfolio_performance(verbose=True)
+
+
+portfolio_weights = pd.DataFrame(p_weights)
+portfolio_weights.index = df.columns
+portfolio_weights.columns = ['weights']
+
+
+df = yf.download(tickers_list, test_start, test_stop)['Adj Close']
+
+w = round(portfolio_weights['weights'],4)
+
+ret = 0
+for i in range(len(portfolio_weights.index)):
+    start = df.iloc[0,i]
+    end = df.iloc[-1,i]
+    gain = end/start
+    ret += gain*w[i]
+    
+    
+ret = round(100*(ret-1)/1, 2)
+print('')
+print (f"Return For Test Period: {ret}%")
+print('')
+
+
